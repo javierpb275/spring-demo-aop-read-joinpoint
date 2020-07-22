797dc237fe4e30c2bc9cb4127813ba000b1e4ea2
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-22 18:54:51 +0200

    added values to the account object

--- src/com/javi/aopdemo/MainDemoApp.java
+++ src/com/javi/aopdemo/MainDemoApp.java
@@ -23,6 +23,9 @@ public class MainDemoApp {
 		
 		// call the business method
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+		
 		//both will match
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
