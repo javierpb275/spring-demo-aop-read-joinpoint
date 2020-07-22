ca08798579c84a3746c696968783291374b69e49
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-22 18:32:16 +0200

    reading method arguments with joinpoints. displayed method signature

--- src/com/javi/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/javi/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.javi.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,15 @@ public class MyDemoLoggingAspect {
 	
 	
 	@Before("com.javi.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")//added fully qualified class name
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		System.out.println("\n=====>>>> Executing @Before advice on method");
+		
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature)  theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		// display method arguments
 	}
 	
 
